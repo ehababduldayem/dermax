2eea2d5ecdfe163e2efc6b41f88b5b6f878bca2b
ehababduldayem <ehababduldayem@gmail.com>
2017-03-31 23:56:26 +0200

    Example and tag classes are built

---
+++ Englishna/Englishna/Models/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Englishna.Models
+{
+    public class Example
+    {
+        public string ExampleStr { get; set; }
+        public ICollection<Tag> Tags{ get; set; }
+        public string Description{ get; set; }
+        public ExampleType ExampleType{ get; set; }
+    }
+}
---
+++ Englishna/Englishna/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Englishna.Models
+{
+    public class Tag
+    {
+        public string TagName { get; set; }
+        public string TagType { get; set; }
+        public string Description { get; set; }
+    }
+}

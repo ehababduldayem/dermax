57921e885089be96785841336e0e84b6e0da16e5
ehababduldayem <ehababduldayem@gmail.com>
2017-03-31 21:04:59 +0200

    My First Commit

---
+++ Englishna/Englishna/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Englishna.Startup))]
+namespace Englishna
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
